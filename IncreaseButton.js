7bd8804450751ddbf6ef691017843668e8d1af73
faenerisis <67673597+faenerisis@users.noreply.github.com>
2023-04-27 11:56:04 +0300

    Create IncreaseButton.js

---
+++ IncreaseButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function Button() {
+  let [counter, setCounter] = useState(0);
+  return(
+    <button onClick={()=> setCounter(counter + 1)}>{counter}</button>
+  );
+}
+
+ReactDOM.render(
+  <Button />,
+  document.getElementById('root')
+);
